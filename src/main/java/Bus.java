c4ef2f06e2f2d19bd5436db60f17907b7d9b935a
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-25 13:56:44 +0100

    finished MVP

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -6,11 +6,21 @@ public class Bus {
     private int capacity;
     private ArrayList<Person> passengers;
 
-    public Bus() {
+    public Bus(String destination, int capacity) {
         this.passengers = new ArrayList<Person>();
+        this.destination = destination;
+        this.capacity = capacity;
     }
 
     public int getPassengers() {
         return this.passengers.size();
+    }
+
+    public void addPassenger(Person person) {
+        this.passengers.add(person);
+    }
+
+    public void removePassenger() {
+        this.passengers.remove(0);
     }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -5,16 +5,31 @@ import static org.junit.Assert.assertEquals;
 
 public class BusTest {
     private Bus bus;
+    private Person person;
 
 
 
     @Before
     public void before() {
-        bus = new Bus();
+        bus = new Bus("Edinburgh", 40);
+        person = new Person();
     }
 
     @Test
     public void hasNoPassengers() {
+        assertEquals(0, bus.getPassengers());
+    }
+
+    @Test
+    public void canAddPassenger() {
+        bus.addPassenger(person);
+        assertEquals(1, bus.getPassengers());
+    }
+
+    @Test
+    public void canRemovePassenger() {
+        bus.addPassenger(person);
+        bus.removePassenger();
         assertEquals(0, bus.getPassengers());
     }
 
